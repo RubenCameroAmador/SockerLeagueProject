21a920471d498724bca63822d623c2fc3ada364d
olondono <olonodono>
2024-05-16 17:02:09 -0500

    Fix: Double click in add button of TeamsPage. Fix: Freeze columns in classification table. Fix: Min value when user add new teams match. Add: Version.

---
+++ SoccerLeague.Client/Models/TeamsMatchModel.cs
@@ -0,0 +1,38 @@
+﻿using SoccerLeague.Core.Entities;
+
+namespace SoccerLeague.Client.Models
+{
+    public class TeamsMatchModel : TeamsMatch
+        {
+            private int scoreTeam1;
+            public new int ScoreTeam1
+            {
+                get => scoreTeam1;
+                set
+                {
+                    if (value == scoreTeam1) return;
+                    scoreTeam1 = Math.Max(value, 0);
+                }
+            }
+            private int scoreTeam2;
+            public new int ScoreTeam2
+            {
+                get => scoreTeam2;
+                set
+                {
+                    if (value == scoreTeam2) return;
+                    scoreTeam2 = Math.Max(value, 0);
+                }
+            }
+
+            public TeamsMatch GetTeamsMatch() => new TeamsMatch
+            {
+                MatchDate = this.MatchDate,
+                IdTeam1 = this.IdTeam1,
+                IdTeam2 = this.IdTeam2,
+                ScoreTeam1 = this.ScoreTeam1,
+                ScoreTeam2 = this.ScoreTeam2
+            };
+       
+    }
+}
